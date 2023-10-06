25b0b9e31019a01e495a6136f6a811185c37739d
Blessdeb <oyemadedeborah@gmail.com>
2023-10-05 17:22:43 -0700

    Tay

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+* main - main function
+*
+* Return: 0 on Success
+*/
+int man(void)
+{
+	system("echo 'and that piece of art is useful" - Dora Korpar, 2015-10-19"');
+	return (1);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-	printf("%s\n", "\"with proper grammar, but the outcome is a piece of art,");
+
+	printf("%s\n", "with proper grammer, but the outcome is a piece of art,");
 	return (0);
 }
