c9c55bbf27617bdf79a3e78257cb170cd2d45a43
Blessdeb <oyemadedeborah@gmail.com>
2023-10-15 18:11:38 -0700

    proramd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,16 +13,15 @@
  */
 void print_number(int n)
 {
-	unsigned int num = n;
 
 	if (n < 0)
 	{
 		putchar('-');
-		num = -num;
+
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
+	if ((n / 10) > 0)
+		print_number(n / 10);
 
-	putchar((num % 10) + '0');
+	putchar((n % 10) + '0');
 }
