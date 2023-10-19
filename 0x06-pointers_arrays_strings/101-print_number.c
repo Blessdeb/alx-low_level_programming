668d5224b660760c845ec1717fdd8feb2fb3e71d
Blessdeb <oyemadedeborah@gmail.com>
2023-10-19 12:17:50 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - print numbers chars
@@ -14,7 +15,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+
 		n1 = -n;
 	}
 
@@ -24,6 +25,7 @@ void print_number(int n)
 	}
 	putchar((n1 % 10) + '0');
 }
+
 
 
 
