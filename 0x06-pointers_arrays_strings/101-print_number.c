32e819463e0a2ff9d47c0ad03013607353d2650a
Blessdeb <oyemadedeborah@gmail.com>
2023-10-19 14:22:57 -0700

    print

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
  * print_number - print numbers chars
@@ -9,20 +8,42 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
-
 	if (n < 0)
 	{
-
-		n1 = -n;
+	putchar('-');
+	n *= -1;
 	}
 
-	if (n1 / 10 != 0)
+	if (n == 0)
 	{
-		print_number(n1 / 10);
+	putchar('0');
+	return;
 	}
-	putchar((n1 % 10) + '0');
+
+	int temp = n;
+	int num_digits = 0;
+
+	while (temp != 0)
+
+	{
+	temp /= 10;
+	num_digits++;
+	}
+
+	while (num_digits > 0)
+
+	{
+	int divisor = 1;
+	for (int i = 1; i < num_digits; i++)
+	divisor *= 10;
+
+	int digit = n / divisor;
+	putchar(digit + '0');
+
+	n %= divisor;
+
+	num_digits--;
+
+	}
 }
 
