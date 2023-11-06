43bfe2e570e8f27203d1ce312a6e0fee6c1342f6
Blessdeb <oyemadedeborah@gmail.com>
2023-11-06 12:46:11 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,35 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - a dog's file dimensions
+ * @name: Storm
+ * @age: float 3.5
+ * @owner: Deb
+ *
+ * Description: Define a new type struct dog with the following elements
+ */
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+	typedef struct dog dog_t;
+
+
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+	char *_strcpy(char *dest, char *src);
+	int _strlen(char *s);
+
+
+#endif
+
