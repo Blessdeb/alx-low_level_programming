5f5d8fa424ed66d451e7322170f4c134ef6f0f10
Blessdeb <oyemadedeborah@gmail.com>
2023-11-06 14:41:50 -0800

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,11 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-		d->name = Storm;
-		d->age = 3.5;
-		d->owner = Deb;
+	if (d != NULL)
+
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 }
 
 
