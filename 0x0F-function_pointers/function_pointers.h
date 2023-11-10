72a2adf6b125a43eba3f6d7f34c11e77e8b570bf
Blessdeb <oyemadedeborah@gmail.com>
2023-11-10 12:37:24 -0800

    pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,8 @@
-#ifndef _function_pointers.h
-#define _function_pointers.h
+#ifndef _FUNCTION_POINTERS
+#define _FUNCTION_POINTERS
+
+#include <stddef.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
